194e6f2f5fa14c1698f6fdfd0bb4ebfbab098c56
John Kluse <j.kluse@gmail.com>
2023-04-27 08:14:18 -0600

    Http Server + Bonus Dynamic path

--- fs-pet-shop/httpServer.js
+++ fs-pet-shop/httpServer.js
@@ -1,11 +1,47 @@
 import http from "http";
-
+import fs from "node:fs";
 import routes from "./routes.js";
 
 let server = http.createServer(function (req, res) {
-	if (routes[req.url] !== undefined) {
-		routes[req.url](req, res);
+	const petRegExp = /^\/pets\/(.*)$/;
+	const hasPetInPath = petRegExp.test(req.url);
+
+	if (hasPetInPath && req.method === "GET") {
+		// --> pets path
+
+		let resource = req.url.match(petRegExp)[1];
+		if (req.url.match(petRegExp)) {
+			fs.readFile("pets.json", "utf-8", function (err, petsJSON) {
+				res.setHeader("Content-Type", "application/json");
+
+				// All pets
+				if (resource === "") {
+					res.end(petsJSON);
+				} else {
+					// pets/num
+					let petsObj = JSON.parse(petsJSON);
+					if (petsObj[resource] === undefined) {
+						returnNotFound();
+					}
+					let petJSON = JSON.stringify(petsObj[+resource]);
+					res.end(petJSON);
+				}
+			});
+		}
 	} else {
+		// --> default case (ie. no valid path)
+		returnNotFound();
+	}
+
+	// if (routes[req.url] !== undefined && req.method === "GET") {
+	// 	routes[req.url](req, res);
+	// } else {
+	// 	res.setHeader("Content-Type", "text/plain");
+	// 	res.statusCode = 404;
+	// 	res.end("Not Found");
+	// }
+
+	function returnNotFound() {
 		res.setHeader("Content-Type", "text/plain");
 		res.statusCode = 404;
 		res.end("Not Found");
