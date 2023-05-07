358866cf3d42ed55749aaf84ce412f418dda52b9
John Kluse <j.kluse@gmail.com>
2023-05-07 17:51:47 -0600

    Added middelware error handling

--- fs-pet-shop/restfulExpressServer.js
+++ fs-pet-shop/restfulExpressServer.js
@@ -11,12 +11,12 @@ const db = new pg.Pool({
 // JSON parser for POST reqs
 server.use(express.json());
 
-server.post("/pets", (req, res) => {
+server.post("/pets", (req, res, next) => {
 	const { age, name, kind } = req.body;
 
 	if (name && kind && Number.isInteger(age)) {
 		db.query("INSERT INTO pet(name, kind, age) VALUES ($1, $2, $3) RETURNING *", [name, kind, age], (err, result) => {
-			if (err) throw err;
+			if (err) next(err);
 			res.status(201).json(result.rows[0]);
 		});
 	} else {
@@ -24,32 +24,30 @@ server.post("/pets", (req, res) => {
 	}
 });
 
-server.get("/pets", (req, res) => {
+server.get("/pets", (req, res, next) => {
 	db.query("SELECT * FROM pet", [], (err, result) => {
-		if (err) throw err;
+		if (err) next(err);
 		res.json(result.rows);
 	});
 });
 
-server.get("/pets/:petIndex", (req, res) => {
+server.get("/pets/:petIndex", (req, res, next) => {
 	let i = Number(req.params.petIndex);
 	if (!Number.isInteger(i)) {
 		res.status(422).send();
 	}
 
 	db.query("SELECT * FROM pet WHERE id = $1", [i], (err, result) => {
-		console.log(result.rows.length);
-		if (err) throw err;
+		if (err) next(err);
 		if (result.rows.length === 0) {
 			res.status(404).send();
 		} else {
-			console.log("has rows");
 			res.json(result.rows[0]);
 		}
 	});
 });
 
-server.patch("/pets/:petIndex", (req, res) => {
+server.patch("/pets/:petIndex", (req, res, next) => {
 	const { age, name, kind } = req.body;
 	let i = Number(req.params.petIndex);
 
@@ -60,7 +58,7 @@ server.patch("/pets/:petIndex", (req, res) => {
 	}
 
 	db.query("UPDATE pet SET age = COALESCE($1, age), kind = COALESCE($2, kind), name = COALESCE($3, name) WHERE id = $4 RETURNING *", [age, kind, name, i], (err, result) => {
-		if (err) throw err;
+		if (err) next(err);
 		if (result.rows.length === 0) {
 			res.sendStatus(404);
 		} else {
@@ -70,11 +68,11 @@ server.patch("/pets/:petIndex", (req, res) => {
 	return;
 });
 
-server.delete("/pets/:petIndex", (req, res) => {
+server.delete("/pets/:petIndex", (req, res, next) => {
 	let i = Number(req.params.petIndex);
 
 	db.query("DELETE FROM pet WHERE id = $1 RETURNING *", [i], (err, result) => {
-		if (err) throw err;
+		if (err) next(err);
 		if (result.rows.length === 0) {
 			res.sendStatus(404);
 		} else {
@@ -85,8 +83,14 @@ server.delete("/pets/:petIndex", (req, res) => {
 });
 
 // default route
-server.get("/*", (req, res) => {
+server.get("/*", (req, res, next) => {
 	res.status(404).send();
+});
+
+// error handling
+server.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Oops, something went wrong!");
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
